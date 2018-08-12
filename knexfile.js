95ba2f4715153fc75441269ece3acddb8f06c121
James Merritt <james@jpmerritt.com.au>
2018-08-13 00:21:30 +1000

    feature: add an init script which creates our database tables and seeds them with test data

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "landchecker-code-sample",
+      user: "jpmerritt",
+      password: ""
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + "/migrations"
+    }
+  }
+};
